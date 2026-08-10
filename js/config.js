213f9fe8f53fcd36fed45fa8333a110726e136b5
bmendez-admin <bmendez@admira.com>
2026-08-10 16:07:08 -0600

    feat:implementacion en github pages

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbwh7ittmItcMDsBZ2MxqU7Iu75TkoidMF_jsXXICsD-RhXL9IDQ-e_6dRVPFToFoGI/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwnEVabnPbFYzFBz_HXMLEOKRqtitILSZXkG2MUduS801hKjnm_dsmyhzrZ6c5JVpHS/exec';
 const API_KEY_PUBLIC = 'boda-publica-2026';
 const DEV_MODE = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost';
 
